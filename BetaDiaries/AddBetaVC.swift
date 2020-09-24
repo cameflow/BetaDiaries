f3597953c5cfa61a8c3f0fedbea2716c3ebb1edb
Alejandro Terrazas <alex.terrazas.z@gmail.com>
2020-09-24 19:39:54 -0400

    Add TableView & screen to add name and description

---
+++ BetaDiaries/AddBetaVC.swift
@@ -0,0 +1,112 @@
+//
+//  AddBetaVC.swift
+//  BetaDiaries
+//
+//  Created by Alejandro Terrazas on 24/09/20.
+//
+
+import UIKit
+
+class AddBetaVC: UIViewController {
+    
+    let betaName        = UITextField()
+    let betaDescription = UITextView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureVC()
+        configureBetaName()
+        configureBetaDescription()
+
+    }
+    
+    private func configureVC() {
+        view.backgroundColor                =   .systemBackground
+        title                               = "Add Beta"
+        navigationItem.rightBarButtonItem   = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addBeta))
+    }
+    
+    private func configureBetaName() {
+        view.addSubview(betaName)
+        betaName.translatesAutoresizingMaskIntoConstraints = false
+        betaName.layer.cornerRadius          = 10
+        betaName.layer.borderWidth           = 2
+        betaName.layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        betaName.textColor                   = .label
+        betaName.tintColor                   = .label
+        betaName.textAlignment               = .center
+        betaName.font                        = UIFont.preferredFont(forTextStyle: .title2)
+        
+        betaName.adjustsFontSizeToFitWidth   = true
+        betaName.backgroundColor             = .tertiarySystemBackground
+        betaName.autocorrectionType          = .no
+        betaName.returnKeyType               = .go
+        betaName.clearButtonMode             = .whileEditing
+        betaName.placeholder                 = "Beta Name"
+        
+        NSLayoutConstraint.activate([
+            betaName.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            betaName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            betaName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            betaName.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func configureBetaDescription() {
+        view.addSubview(betaDescription)
+        betaDescription.translatesAutoresizingMaskIntoConstraints = false
+        betaDescription.layer.cornerRadius  = 10
+        betaDescription.layer.borderWidth   = 2
+        betaDescription.layer.borderColor   = UIColor.systemGray4.cgColor
+        betaDescription.delegate            = self
+        
+        betaDescription.textColor           = .lightGray
+        betaDescription.tintColor           = .label
+        betaDescription.textAlignment       = .left
+        betaDescription.font                = UIFont.preferredFont(forTextStyle: .title2)
+        
+        betaDescription.backgroundColor     = .tertiarySystemBackground
+        betaDescription.isEditable          = true
+        betaDescription.text                = "Beta Description..."
+        betaDescription.autocorrectionType  = .no
+        
+        NSLayoutConstraint.activate([
+            betaDescription.topAnchor.constraint(equalTo: betaName.bottomAnchor, constant: 100),
+            betaDescription.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            betaDescription.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            betaDescription.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+    
+    @objc func addBeta(){
+        print(betaName.text ?? "No title")
+        print(betaDescription.text ?? "No description")
+        dismiss(animated: true)
+    }
+    
+
+}
+
+extension AddBetaVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView)
+    {
+        if (textView.text == "Beta Description..." && textView.textColor == .lightGray)
+        {
+            textView.text = ""
+            textView.textColor = .label
+        }
+        textView.becomeFirstResponder()
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView)
+    {
+        if (textView.text == "")
+        {
+            textView.text = "Beta Description..."
+            textView.textColor = .lightGray
+        }
+        textView.resignFirstResponder()
+    }
+}
--- BetaDiaries/ViewController.swift
+++ BetaDiaries/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    
+    
+    let tableView = UITableView()
+    var betas:[String]  = []
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureVC()
+        configureTableView()
+    }
+    
+    private func configureVC() {
+        view.backgroundColor = .systemBackground
+        title = "Beta Diaries"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addBeta))
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 50
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "BetaCell")
+    }
+    
+    @objc func addBeta() {
+        print("Betta Added")
+        betas.append("Beta # \(counter)")
+        counter += 1
+        tableView.reloadData()
+        let destVC = AddBetaVC()
+        let navController = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return betas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BetaCell")!
+        cell.textLabel?.text = betas[indexPath.row]
+        return cell
+    }
+    
+
+}
